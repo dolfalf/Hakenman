2b7a14c0115fcf3160ceedef2f917c32b19a60fd
俊河 李 <toshikawa@shunga-no-MacBook-Pro.local>
2014-07-02 19:43:50 +0900

    ファイル追加

---
+++ Hakenman/screen/menu/LeftTableViewData.m
@@ -0,0 +1,13 @@
+//
+//  LeftTableViewData.m
+//  Hakenman
+//
+//  Created by 俊河 李 on 2014/07/02.
+//  Copyright (c) 2014年 kjcode. All rights reserved.
+//
+
+#import "LeftTableViewData.h"
+
+@implementation LeftTableViewData
+
+@end
